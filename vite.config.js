420a14301f65e63e2673ef1d6e1398e7e901eb6e
GouthamiJukanti <gouthamijukanti6@gmail.com>
2025-02-06 11:44:02 +0530

    Old to new in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Wordle-Clone-Old/",
+  base: "/Wordle-Clone-New/",
   plugins: [react()],
   server: {
     host: true,
